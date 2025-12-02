5e461f9e5f3644d6f369d30e15780a5d862e9b5d
Aswin Reji <aswinreji.004@gmail.com>
2025-12-02 17:10:17 +0000

    feat: refractor whole code and start from begining
     we created a ui from scratch using deepseek for ui design and removed the whole file for the update and we can view them from previous commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <Router>
+      <div className="min-h-screen bg-base-100 flex flex-col">
+        
+      </div>
+    </Router>
+  )
+}
+
+export default App
